a3e1aa31215ffa920b4616135d263d3daa49e530
Maria-source-cell <manuelabirnat@gmail.com>
2025-08-05 20:38:02 +0200

    Refactor calculator logic and add backspace button

--- script.js
+++ script.js
@@ -1,16 +1,59 @@
-const display = document.getElementById("display");
+ const display = document.getElementById("display");
+
+ let currentInput = "";
+ let firstNumber = null;
+ let operator = null;
+ let shouldResetDisplay = false;
+
 
 function appendNumber(number){
-    display.value += number;
+    if (shouldResetDisplay) {
+        console.log("reset input after operator")
+        currentInput = "";
+        shouldResetDisplay = false;
+    }
+    currentInput += number;
+    display.value = currentInput;
+
+    console.log(`nr. pressed: ${number}, currentInput: ${currentInput}` )
 }
 
 function clearDisplay() {
+    console.log("Clearing display and reset")
+    currentInput = "";
+    firstNumber = null;
+    operator = null;
+    shouldResetDisplay = false;
     display.value ="";
 }
 
+function setOperator(op) {
+    if (operator !== null) calculate();
+
+    firstNumber = parseFloat(currentInput);
+    operator = op;
+    shouldResetDisplay = true;
+}
+
 function calculate() {
+    if (operator === null || shouldResetDisplay) return;
+
+    const secondNumber = parseFloat(currentInput);
+    console.log(`Calculating: ${firstNumber} ${operator} ${secondNumber}`);
+
+    const result = operate(operator, firstNumber, secondNumber)
+
+    console.log(`Result: ${result}`)
+
+    display.value = result;
+    currentInput = result.toString();
+    operator = null;
+    firstNumber = null;
+    shouldResetDisplay = true;
 
 }
+
+
 function add(x, y) {
     return x + y;
 }
@@ -25,7 +68,7 @@ function multiply(x, y) {
 
 function divide(x,y) {
     if (y === 0) {
-        return "cannot divide by zero";
+        return "Nice try. You can't divide by 0 🙃";
     }
     return x / y;
 }
